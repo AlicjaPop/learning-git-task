9070b9a45fa4d0c69be62d09390826d3150d42fa
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-02-19 19:22:55 +0100

    capitalized products names

--- Zadanie_3_3.py
+++ Zadanie_3_3.py
@@ -8,6 +8,9 @@ print("Lista Zakupów")
 product_list=[]
 for shop, product in shops_dict.items():
     Shop=shop.capitalize()
-    print(f"Idę do {Shop}, kupuję tu następujące rzeczy:", shops_dict[shop])
-    product_list=product_list + shops_dict[shop]
+    Cap_product_list=[]
+    for Product in product:
+        Cap_product_list.append(Product.capitalize())
+    print(f"Idę do {Shop}, kupuję tu następujące rzeczy:", Cap_product_list)
+    product_list=product_list + product
 print(f"W sumie kupuję", len(product_list), "produktów.")
